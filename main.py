849fd8c22343377b8d515dc8aea860802e348878
Joseph avila <josdavil@espol.edu.ec>
2021-04-17 16:06:09 -0500

    add:Perlin Noise 3D

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pmpy 
 import matplotlib.pyplot as plt
-
-im=pmpy.perlin_noise.generate_fractal_noise_2d([2000,2000],[10,10],porosity=0.55)
+im=pmpy.perlin_noise.generate_fractal_noise_2d([100,100],[10,10],porosity=0.55)
 plt.show(block=False)
 plt.imshow(im)
 plt.pause(1000)
